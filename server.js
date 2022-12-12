3cb0158305a7cf89d6052dd00d628b955231cbc9
fakhripraya <fakhri@indomaret.co.id>
2022-12-12 14:31:21 +0700

    add cors to ws

--- server.js
+++ server.js
@@ -2,20 +2,28 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const app = express();
-app.use(cors());
 const server = http.createServer(app);
 const socket = require("socket.io");
-const io = socket(server);
+const io = socket(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: ["user-agent"],
+        credentials: true,
+    },
+});
 
+app.use(cors());
 const rooms = {};
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
     socket.on("join room", (userJoin, roomCode) => {
+        console.log("user join nih: " + userJoin.id);
         if (!rooms[roomCode]) rooms[roomCode] = [];
-        if (rooms[roomCode].find(user => user.id === userJoin.id)) return;
+        if (rooms[roomCode].find((user) => user.id === userJoin.id)) return;
 
         rooms[roomCode].push({ ...userJoin, socketId: socket.id });
-        const otherUser = rooms[roomCode].find(user => user.id !== userJoin.id);
+        const otherUser = rooms[roomCode].find((user) => user.id !== userJoin.id);
 
         if (otherUser) {
             socket.emit("user call", otherUser.socketId);
@@ -23,18 +31,18 @@ io.on("connection", socket => {
         }
     });
 
-    socket.on("offer", payload => {
+    socket.on("offer", (payload) => {
         io.to(payload.target).emit("offer", payload);
     });
 
-    socket.on("answer", payload => {
+    socket.on("answer", (payload) => {
         io.to(payload.target).emit("answer", payload);
     });
 
-    socket.on("ice-candidate", incoming => {
+    socket.on("ice-candidate", (incoming) => {
         io.to(incoming.target).emit("ice-candidate", incoming.candidate);
     });
 });
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 server.listen(8000, () => console.log(`server is running on port ${port}`));
