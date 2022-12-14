22387b4266dfe39ab9b757a81f2b4b09c892ba66
fakhripraya <fakhri@indomaret.co.id>
2022-12-14 22:20:02 +0700

    change from crypto to uuidv4

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const { ROOM_FULL, ROOM_AVAILABLE, USER_ALREADY_JOIN, ROOM_UNAVAILABLE } = require("./variables/global");
+const {
+    ROOM_FULL,
+    ROOM_AVAILABLE,
+    USER_ALREADY_JOIN,
+    ROOM_UNAVAILABLE,
+} = require("./variables/global");
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
-const { randomUUID } = require("crypto");
+const { v4 } = require("uuid");
 const io = socket(server, {
     cors: {
         origin: "*",
@@ -21,14 +26,15 @@ const rooms = [];
 
 io.on("connection", (socket) => {
     socket.on("join room", (userJoin, roomCode) => {
-
         if (rooms[roomCode].users.find((user) => user.id === userJoin.id)) return;
         if (rooms[roomCode].users.length >= 2) socket.emit("rdp error", ROOM_FULL);
 
         socket.joinedRoom = roomCode;
         rooms[roomCode].users.push({ ...userJoin, socketId: socket.id });
 
-        const otherUser = rooms[roomCode].users.find((user) => user.id !== userJoin.id);
+        const otherUser = rooms[roomCode].users.find(
+            (user) => user.id !== userJoin.id
+        );
         if (otherUser) {
             socket.emit("user call", otherUser.socketId);
             socket.to(otherUser.socketId).emit("user joined", socket.id);
@@ -50,7 +56,9 @@ io.on("connection", (socket) => {
     socket.on("disconnecting", () => {
         if (!rooms[socket.joinedRoom]) return;
 
-        const userSocket = rooms[socket.joinedRoom].users.find((user) => user.socketId === socket.id);
+        const userSocket = rooms[socket.joinedRoom].users.find(
+            (user) => user.socketId === socket.id
+        );
         if (!userSocket) return;
 
         const socketIndex = rooms[socket.joinedRoom].users.indexOf(userSocket);
@@ -59,8 +67,7 @@ io.on("connection", (socket) => {
 });
 
 // GET method route
-app.get('/v1/room/check', (req, res) => {
-
+app.get("/v1/room/check", (req, res) => {
     // check query param availability
     if (!req.query) return res.sendStatus(400);
 
@@ -69,29 +76,47 @@ app.get('/v1/room/check', (req, res) => {
         user: {
             id: req.query.userId,
         },
-        roomCode: req.query.roomCode
+        roomCode: req.query.roomCode,
     };
 
-    if (!rooms[query.roomCode]) return res.send(JSON.stringify({
-        code: ROOM_UNAVAILABLE
-    })).status(200);
+    if (!rooms[query.roomCode])
+        return res
+            .send(
+                JSON.stringify({
+                    code: ROOM_UNAVAILABLE,
+                })
+            )
+            .status(200);
 
-    if (rooms[query.roomCode].users.find((user) => user.id === query.user.id)) return res.send(JSON.stringify({
-        code: USER_ALREADY_JOIN
-    })).status(403);
+    if (rooms[query.roomCode].users.find((user) => user.id === query.user.id))
+        return res
+            .send(
+                JSON.stringify({
+                    code: USER_ALREADY_JOIN,
+                })
+            )
+            .status(403);
 
-    if (rooms[query.roomCode].users.length >= 2) return res.send(JSON.stringify({
-        code: ROOM_FULL
-    })).status(403);
+    if (rooms[query.roomCode].users.length >= 2)
+        return res
+            .send(
+                JSON.stringify({
+                    code: ROOM_FULL,
+                })
+            )
+            .status(403);
 
-    return res.send(JSON.stringify({
-        code: ROOM_AVAILABLE
-    })).status(200);
-})
+    return res
+        .send(
+            JSON.stringify({
+                code: ROOM_AVAILABLE,
+            })
+        )
+        .status(200);
+});
 
 // POST method route
-app.post('/v1/room/create', (req, res) => {
-
+app.post("/v1/room/create", (req, res) => {
     // check request body availability
     if (!req.body) return res.sendStatus(400);
 
@@ -99,18 +124,22 @@ app.post('/v1/room/create', (req, res) => {
     const user = req.body.user;
 
     // create random crypted uuid
-    const roomCode = randomUUID();
+    const roomCode = v4();
 
     rooms[roomCode] = {
         users: [],
         created_at: new Date(),
-        created_by: user.id
+        created_by: user.id,
     };
 
-    return res.send(JSON.stringify({
-        roomCode: roomCode
-    })).status(200);
-})
+    return res
+        .send(
+            JSON.stringify({
+                roomCode: roomCode,
+            })
+        )
+        .status(200);
+});
 
 const port = process.env.PORT || 8000;
 server.listen(8000, () => console.log(`server is running on port ${port}`));
